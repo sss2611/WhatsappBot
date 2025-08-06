9ed06ccf3f9b428e4d43c9855fbdd966d9b4eae8
sss2611 <solss2611@gmail.com>
2025-08-06 10:45:54 -0300

    link de Facebook

--- bot/messageHandler.js
+++ bot/messageHandler.js
@@ -4,8 +4,8 @@ module.exports = (message, client) => {
   if (text === 'hola') {
     message.reply('¡Hola! ¿En qué puedo ayudarte?');
   } else if (text === 'info') {
-    message.reply('Puedes consultar nuestro sitio web para más información.');
+    message.reply('Puedes visitar nuestro Facebook para mas información: https://www.facebook.com/groups/507296329349636/user/100026735442194/?locale=es_LA.');
   } else if (text.includes('precio')) {
     message.reply('Nuestros precios varían según el producto. ¿Qué te interesa?');
   }
-};
+};s
